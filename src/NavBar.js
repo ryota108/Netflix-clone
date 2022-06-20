94bcc752e8a50436afb71eb36af0801e78671d1e
Ryota Kodama <aya.ryo.718@outlook.jp>
2022-06-20 13:48:09 +0900

    ロゴの変更

--- src/NavBar.js
+++ src/NavBar.js
@@ -1,5 +1,6 @@
 import React, { useEffect,useState } from "react";
 import classes from "./NavBar.module.css";
+import logo from "./Image/logo.png";
 
 function NavBar() { 
  const [show,handleShow] =useState(false);
@@ -9,7 +10,7 @@ function NavBar() {
     <div className={`${classes.nav} ${show && classes.navBlack}`}>
       <img
         className={classes.navLogo}
-        src="https://upload.wikimedia.org/wikipedia/commons/thumb/0/08/Netflix_2015_logo.svg/340px-Netflix_2015_logo.svg.png"
+        src={logo}
         alt="Netflix logo"
       />
       <img
